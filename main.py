65263571fc90cf123e27ab8ce2d544554acfdf32
oboy21 <oboy11b@gmail.com>
2024-11-06 13:17:52 +0300

    implementing dicegame class

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Die:
         return new_value
 
 
-class PLayer:
+class Player:
     def __init__(self, die, is_computer=False):
         self._die = die
         self._is_computer = is_computer
@@ -43,4 +43,73 @@ class PLayer:
     def roll_die(self):
         return self._die.roll()
 
+class DiceGame:
 
+
+    def __init__(self, player, computer):
+        self._player = player
+        self._computer = computer
+
+    def play(self):
+        print("===================================")
+        print("🎲 Welcome to Roll the Dice!")
+        print("===================================")
+        while True:
+            self.play_round()
+            #Todo:implement game over
+
+    def play_round(self):
+        #Welcome the user
+        self.print_round_welcome()
+
+        # Roll the dice
+        player_value = self._player.roll_die()
+        computer_value = self._computer.roll_die()
+
+        #Show the values
+        self.show_dice(player_value, computer_value)
+
+
+        #DEtermine winner and loser
+        if player_value > computer_value:
+            print("You won the round!")
+            self.update_counters(winner=self._player, loser=self._computer)
+        elif computer_value > player_value:
+            print("The computer won this round. Try again.")
+            self.update_counters(winner=self._computer, loser=self._player)
+
+        else:
+            print("It's a tie!")
+
+
+        #Show counters
+
+    def print_round_welcome(self):
+        print("--------- New Round-----------")
+        input(" 🎲 Press any key to roll the dice. 🎲")
+
+
+    def show_dice(self, player_value, computer_value):
+        print(f"Your die: {player_value}")
+        print(f"Computer die: {computer_value}")
+
+    def update_counters(self, winner, loser):
+        winner.decrement_counter()
+        loser.increment_counter()
+
+    def show_counters(self):
+        print(f"Your counter: {self._player.counter}")
+        print(f"Computer counter: {self._computer.counter}")
+
+
+#Create instances
+player_die = Die()
+computer_die = Die()
+
+my_player = Player(player_die, is_computer=False)
+computer_player = Player(computer_die, is_computer=True)
+
+game = DiceGame(my_player, computer_player)
+
+#Start the game
+game.play()
